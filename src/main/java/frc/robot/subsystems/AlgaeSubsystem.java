6612638dd8b6586b4a80bd35db771a76eb241f20
joecoleman <jcoleman@haleschools.net>
2025-03-03 10:27:23 -0500

    commitw

--- src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
@@ -10,10 +10,8 @@ import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotController;
@@ -38,8 +36,8 @@ public class AlgaeSubsystem extends SubsystemBase {
 
   // Initialize intake SPARK. We will use open loop control for this so we don't need a closed loop
   // controller like above.
-  private SparkFlex intakeMotor =
-      new SparkFlex(AlgaeSubsystemConstants.kIntakeMotorCanId, MotorType.kBrushless);
+  private SparkMax intakeMotor =
+      new SparkMax(AlgaeSubsystemConstants.kIntakeMotorCanId, MotorType.kBrushless);
 
   // Member variables for subsystem state management
   private boolean stowWhenIdle = true;
